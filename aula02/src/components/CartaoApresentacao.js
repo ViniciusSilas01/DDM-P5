e937b5a02f810ffd055d10f587be0dfdf0e23803
ViniciusSilas01 <vinicius2012silas@gmail.com>
2025-09-23 16:18:50 -0300

    enriquecendo material aula02

---
+++ aula02/src/components/CartaoApresentacao.js
@@ -0,0 +1,86 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const CartaoApresentacao = () => {
+  return (
+    <View style={styles.container}>        
+        {/* IMAGEM DE PERFIL */}  
+        <Image style={styles.imagemPerfil}
+          source={{ uri: 'https://avatars.githubusercontent.com/u/1?v=4'}}/>        
+          
+        {/* NOME */}
+        <Text style={styles.nome}>john Doe</Text>
+
+        {/* DESCRICAO */}
+        <Text style={styles.descricao}>Desenvolvedor Mobile | Apaixonado por Tecnologia </Text>
+
+        {/* BOTOES DE CONTATO */}
+      <View style={styles.botoesContainer}>
+        <TouchableOpacity style={[styles.botao, styles.botaoGithub]}>
+          <Text style={styles.textoBotao}>GitHub</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.botao, styles.botaoLinkedln]}>
+          <Text style={styles.textoBotao}>Linkedln</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 300,
+        padding: 20,
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 5,
+    }, 
+    imagemPerfil: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 10,
+    },
+    nome: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#333',
+    },
+    descricao: {
+        fontSize: 14,
+        color: '#666',
+        textAlign: 'center',
+        marginVertical: 10,
+    },
+    botao: {
+        flex: 1,
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginHorizontal: 5,
+    },
+    botaoGithub: {
+        backgroundColor: '#333'
+    },
+    botaoLinkedln: {
+        backgroundColor: '#0077b5'
+    },
+    textoBotao: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    botoesContainer: {
+    flexDirection: 'row', 
+    marginTop: 10
+    }
+})
+
+export default CartaoApresentacao
